c6f0b7930aee96e9be40c9bd02fcfd141f027263
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2025-10-27 13:53:32 +0200

    Lagt till endpoints för api-anrop till användare (get/add/update/delete)

--- app/Http/Controllers/Api/V1/UserApiController.php
+++ app/Http/Controllers/Api/V1/UserApiController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Repositories\Interfaces\UserRepo;
+use Illuminate\Http\Request;
 
 class UserApiController extends Controller {
     public function __construct(private UserRepo $repo) {}
@@ -11,5 +13,47 @@ class UserApiController extends Controller {
     public function all() {
         $lista = $this->repo->all();
         return response()->json(['users' => $lista]);
+    }
+
+    public function get(Request $request) {
+        try {
+            $id = filter_var($request->route('id'), FILTER_VALIDATE_INT);
+            $item = $this->repo->get($id);
+            return response()->json(['user' => $item]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
+    }
+
+    public function add(Request $request) {
+        try {
+            $user = User::factory()->make($request->input());
+            $this->repo->add($user);
+            return response()->json(['user' => $user], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
+    }
+
+    public function update(Request $request) {
+        try {
+            $id = filter_var($request->route('id'), FILTER_VALIDATE_INT);
+            $user = $this->repo->get($id);
+            $user->fill($request->all());
+            $this->repo->update($user);
+            return response()->json(['user' => $user]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
+    }
+
+    public function remove(Request $request) {
+        try {
+            $id = filter_var($request->input('id'), FILTER_VALIDATE_INT);
+            $this->repo->delete($id);
+            return response()->json(null, 204);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 401);
+        }
     }
 }
